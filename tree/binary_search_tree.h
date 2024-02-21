0aae4d5a0ed95f3fd4d5eba73db20faa0e1d30b0
rogerg6 <rogerg6@163.com>
2024-02-21 11:43:49 +0800

    add binary_search_tree

---
+++ tree/binary_search_tree.h
@@ -0,0 +1,188 @@
+
+#ifndef _BINARY_SEARCH_TREE_H
+#define _BINARY_SEARCH_TREE_H
+
+#include "linked_list_queue.h"
+#include <stdexcept>
+
+template<typename T>
+struct BSNode {
+    T       val;
+    BSNode *left;
+    BSNode *right;
+    BSNode(T v)
+        : val(v)
+        , left(nullptr)
+        , right(nullptr) {}
+};
+
+template<typename T>
+class BinarySearchTree {
+public:
+    BinarySearchTree()
+        : _root(nullptr)
+        , _size(0) {}
+
+    ~BinarySearchTree() {
+        // while (_root) Remove(_root->val);
+        destroy(_root);
+        _root = nullptr;
+        _size = 0;
+    }
+
+    int Size() const { return _size; }
+
+    void Insert(const T val) {
+        BSNode<T>  *node = _root;
+        BSNode<T> **pre  = &_root;   // node的父节点的left/right的地址
+        // find the place to insert
+        while (node) {
+            if (node->val == val)
+                return;
+            else if (val < node->val) {
+                pre  = &(node->left);
+                node = node->left;
+            } else {
+                pre  = &(node->right);
+                node = node->right;
+            }
+        }
+        BSNode<T> *newnode = new BSNode<T>(val);
+        if (!newnode) throw std::runtime_error("Failed to new Node.");
+        *pre = newnode;
+        _size++;
+    }
+
+    void Remove(const T val) {
+        BSNode<T>  *node  = _root;    // node是待删除的点
+        BSNode<T> **child = &_root;   // node的父节点的child成员的地址
+
+        // find
+        while (node) {
+            if (node->val == val)
+                break;
+            else if (val < node->val) {
+                child = &(node->left);
+                node  = node->left;
+            } else {
+                child = &(node->right);
+                node  = node->right;
+            }
+        }
+        if (!node) return;   // not found
+
+        if (node->left == nullptr && node->right == nullptr) {
+            // leaf节点
+            delete node;
+            *child = nullptr;
+            _size--;
+        } else if (node->left == nullptr && node->right) {
+            // 只有right子树. 交换left子树最大的值和node的值, 删除left子树最大值的节点
+            BSNode<T> *rn = node->right;
+            while (rn->left) {
+                rn = rn->left;
+            }
+
+            T tmp = rn->val;
+            Remove(tmp);
+            node->val = tmp;
+        } else {
+            /*
+             * 2case: only left_tree || 有left_tree和right_tree.
+             * 2种情况操作一样
+             *
+             * 度=2. 选取left_tree最大值和right_tree最小值都可以.
+             * 这里我们选left_tree.max
+             * 交换left子树最大的值和node的值, 删除left子树最大值的节点
+             */
+            BSNode<T> *ln = node->left;
+            while (ln->right) {
+                ln = ln->right;
+            }
+
+            T tmp = ln->val;
+            Remove(tmp);
+            node->val = tmp;
+        }
+    }
+
+    BSNode<T> *Find(const T val) const {
+        BSNode<T> *node = _root;
+        while (node) {
+            if (node->val == val)
+                return node;
+            else if (val < node->val)
+                node = node->left;
+            else
+                node = node->right;
+        }
+        return nullptr;
+    }
+
+    std::vector<T> LevelOrderTraverse() const {
+        std::vector<T> order;
+        if (!_root) return std::vector<T>{};
+        LinkedListQueue<BSNode<T> *> q;
+        q.Push(_root);
+
+        while (!q.Empty()) {
+            BSNode<T> *node = q.Pop();
+            order.push_back(node->val);
+
+            BSNode<T> *l = node->left;
+            if (l) q.Push(l);
+            BSNode<T> *r = node->right;
+            if (r) q.Push(r);
+        }
+
+        return order;
+    }
+    std::vector<T> PreOrderTraverse() const {
+        std::vector<T> ret;
+        doPreOrder(_root, ret);
+        return ret;
+    }
+    std::vector<T> InOrderTraverse() const {
+        std::vector<T> ret;
+        doInOrder(_root, ret);
+        return ret;
+    }
+    std::vector<T> PostOrderTraverse() const {
+        std::vector<T> ret;
+        doPostOrder(_root, ret);
+        return ret;
+    }
+
+private:
+    void doPreOrder(BSNode<T> *root, std::vector<T> &order) const {
+        if (!root) return;
+        order.push_back(root->val);
+        doPreOrder(root->left, order);
+        doPreOrder(root->right, order);
+    }
+    void doInOrder(BSNode<T> *root, std::vector<T> &order) const {
+        if (!root) return;
+        doInOrder(root->left, order);
+        order.push_back(root->val);
+        doInOrder(root->right, order);
+    }
+    void doPostOrder(BSNode<T> *root, std::vector<T> &order) const {
+        if (!root) return;
+        doPostOrder(root->left, order);
+        doPostOrder(root->right, order);
+        order.push_back(root->val);
+    }
+
+    // 销毁树
+    void destroy(BSNode<T> *root) {
+        if (!root) return;
+        destroy(root->left);
+        destroy(root->right);
+        delete root;
+    }
+
+    BSNode<T> *_root;
+    int        _size;
+};
+
+#endif
--- unittest/test_tree.cc
+++ unittest/test_tree.cc
@@ -1,5 +1,6 @@
 
 #include "array_binary_tree.h"
+#include "binary_search_tree.h"
 #include "binary_tree.h"
 #include "gtest/gtest.h"
 
@@ -87,4 +88,61 @@ TEST(array_binary_tree, traverse) {
     t.order.clear();
     t.PostOrderTraverse();
     EXPECT_EQ(t.order, post_order);
+}
+
+/**
+ * 测试各种二叉树的遍历顺序是否正确
+ * TREE遍历接口必须一致, T是TREE中保存的值的类型
+ */
+template<typename TREE, typename T>
+void test_traverse(const TREE &t, const std::vector<T> level_order, const std::vector<T> pre_order,
+                   const std::vector<T> in_order, const std::vector<T> post_order) {
+    EXPECT_EQ(t.LevelOrderTraverse(), level_order);
+    EXPECT_EQ(t.PreOrderTraverse(), pre_order);
+    EXPECT_EQ(t.InOrderTraverse(), in_order);
+    EXPECT_EQ(t.PostOrderTraverse(), post_order);
+}
+
+TEST(binary_search_tree, function_test) {
+    BinarySearchTree<int> t;
+    std::vector<int>      nums = {5, 4, 7, 2, 6, 9, 1, 3, 8, 10};
+    for (auto n : nums) t.Insert(n);
+    for (auto n : nums) EXPECT_TRUE(t.Find(n) != nullptr);
+    EXPECT_TRUE(t.Find(11) == nullptr);
+    EXPECT_EQ(t.Size(), 10);
+    test_traverse<BinarySearchTree<int>, int>(t,
+                                              nums,
+                                              {5, 4, 2, 1, 3, 7, 6, 9, 8, 10},
+                                              {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                                              {1, 3, 2, 4, 6, 8, 10, 9, 7, 5});
+    t.Remove(6);   // remove leaf node
+    EXPECT_EQ(t.Size(), 9);
+    test_traverse<BinarySearchTree<int>, int>(t,
+                                              {5, 4, 7, 2, 9, 1, 3, 8, 10},
+                                              {5, 4, 2, 1, 3, 7, 9, 8, 10},
+                                              {1, 2, 3, 4, 5, 7, 8, 9, 10},
+                                              {1, 3, 2, 4, 8, 10, 9, 7, 5});
+    t.Remove(7);   // remove node has right tree
+    EXPECT_EQ(t.Size(), 8);
+    test_traverse<BinarySearchTree<int>, int>(t,
+                                              {5, 4, 8, 2, 9, 1, 3, 10},
+                                              {5, 4, 2, 1, 3, 8, 9, 10},
+                                              {1, 2, 3, 4, 5, 8, 9, 10},
+                                              {1, 3, 2, 4, 10, 9, 8, 5});
+    t.Remove(4);   // remove node has left tree
+    EXPECT_EQ(t.Size(), 7);
+    test_traverse<BinarySearchTree<int>, int>(t,
+                                              {5, 3, 8, 2, 9, 1, 10},
+                                              {5, 3, 2, 1, 8, 9, 10},
+                                              {1, 2, 3, 5, 8, 9, 10},
+                                              {1, 2, 3, 10, 9, 8, 5});
+    t.Insert(4);
+    EXPECT_EQ(t.Size(), 8);
+    t.Remove(5);   // remove node has two child tree
+    EXPECT_EQ(t.Size(), 7);
+    test_traverse<BinarySearchTree<int>, int>(t,
+                                              {4, 3, 8, 2, 9, 1, 10},
+                                              {4, 3, 2, 1, 8, 9, 10},
+                                              {1, 2, 3, 4, 8, 9, 10},
+                                              {1, 2, 3, 10, 9, 8, 4});
 }
